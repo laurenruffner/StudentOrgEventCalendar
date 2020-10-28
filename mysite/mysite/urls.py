982252ffb34c3b14fd4b213274a75f98e21e8623
lauren-ruffner11 <lauren.ruffner11@tamu.edu>
2020-10-28 16:34:23 -0500

    Added models

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('eventviewer/', include('eventviewer.urls')),
+    path('', include('eventviewer.urls')),
     path('admin/', admin.site.urls),
 ]
