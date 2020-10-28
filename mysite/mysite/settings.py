3a6719c701a89490e2961855ee5db5fd87b13d98
lauren-ruffner11 <lauren.ruffner11@tamu.edu>
2020-10-28 16:41:44 -0500

    Added Migrations

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'eventviewer.apps.EventviewerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
