7067e6f25d8137d583b1ff3d5a4ea3ae0d5fb0b0
lauren-ruffner11 <lauren.ruffner11@tamu.edu>
2020-10-28 16:36:39 -0500

    Added models

--- mysite/eventviewer/models.py
+++ mysite/eventviewer/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Organization(models.Model):
+    org_name = models.CharField(max_length = 200)
+    org_url = models.CharField(max_length = 200)
+    org_description = models.CharField(max_length = 200)
+    org_category = models.CharField(max_length = 200)
+
+class Event(models.Model):
+    organizaiton = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    event_name = models.CharField(max_length = 200)
+    event_datetime = models.DateTimeField('Start date and time of event')
+    event_description = models.CharField(max_length = 200)
